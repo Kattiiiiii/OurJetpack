6486fce5b73246b54d0c69428ffbd53764b86356
Kattiiiiii <katikalonline@gmail.com>
2022-07-21 15:00:58 +0200

    Create Jet_Game.py

---
+++ Jet_Game.py
@@ -0,0 +1,28 @@
+#load images
+#pyg.image.load('image_name.png')
+
+import pygame as pyg
+pyg.init()
+
+
+#initialize game screen
+screen = pyg.display.set_mode((400,300))
+pyg.display.set_caption('Jumping Jet')
+
+#Game loop
+
+#keep game running till user quit the game
+playingGame = True
+while playingGame:
+
+    #check for events in queue
+    for event in pyg.event.get():
+        #if game is of type quit, quit game  
+        if event.type == pyg.QUIT:  
+           playingGame = False
+
+    #defining background color
+    color = (255,255,0)
+    screen.fill(color)
+    pyg.display.flip()
+
